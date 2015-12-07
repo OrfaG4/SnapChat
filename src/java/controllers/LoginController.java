8fedd23575c4f29da8f4e589ec6b303549236653
J0eLGK <GEORGE_TAME@HOTMAIL.COM>
2015-12-07 03:27:38 +0200

    Update LoginController.java

--- src/java/controllers/LoginController.java
+++ src/java/controllers/LoginController.java
@@ -2,6 +2,10 @@ package controllers;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -12,8 +16,9 @@ import javax.servlet.http.HttpSession;
 
 public class LoginController extends HttpServlet {
         protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException  {
+            throws ServletException, IOException, SQLException  {
             response.setContentType("text/html;charset=UTF-8");
+            ArrayList<User> friends = new ArrayList<User>();
             PrintWriter pw = response.getWriter();
             try {
                 if(User.LoginUser(request.getParameter("username"),request.getParameter("password"))){
@@ -24,6 +29,8 @@ public class LoginController extends HttpServlet {
                     HttpSession sessionUser = request.getSession();
                     sessionUser.setAttribute("username",user.getUsername());
                     sessionUser.setAttribute("id", user.getId());
+                    friends = user.getAllMyFriends();
+                    request.setAttribute("friends", friends);
                     RequestDispatcher rd1 = request.getRequestDispatcher("views/home.jsp");
                     rd1.forward(request,response);     
                 }  
@@ -39,13 +46,21 @@ public class LoginController extends HttpServlet {
         @Override
         protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-            processRequest(request, response);
+            try {
+                processRequest(request, response);
+            } catch (SQLException ex) {
+                Logger.getLogger(LoginController.class.getName()).log(Level.SEVERE, null, ex);
+            }
         }
         
         @Override
         protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-            processRequest(request, response);
+            try {
+                processRequest(request, response);
+            } catch (SQLException ex) {
+                Logger.getLogger(LoginController.class.getName()).log(Level.SEVERE, null, ex);
+            }
         }
         
 
