88f996a28845e98def6cfe589cba468b0d9e8045
kostasprod <kostasprod@hotmail.com>
2015-12-07 03:22:31 +0200

    Update HomeRouter.java

--- src/java/routers/HomeRouter.java
+++ src/java/routers/HomeRouter.java
@@ -7,15 +7,20 @@ package routers;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import models.User;
 
 public class HomeRouter extends HttpServlet{
-    protected void home(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException{
+    protected void home(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException, SQLException{
         response.setContentType("text/html;charset=UTF-8");
         HttpSession s = request.getSession();
         PrintWriter pw = response.getWriter();
@@ -23,6 +28,12 @@ public class HomeRouter extends HttpServlet{
             pw.println("Δέν έχετε δικαίωμα πρόσβασης");
             pw.println("<a href='index'>Συνδεθείτε</a>");
         }else{
+            ArrayList<User> friends = new ArrayList<User>();
+            User user = new User();
+            user.setUsername(s.getAttribute("username").toString());
+            user.getUser();
+            friends = user.getAllMyFriends();
+            request.setAttribute("friends", friends);
             RequestDispatcher rd1 = request.getRequestDispatcher("views/home.jsp");
             rd1.forward(request,response);
         }
@@ -31,13 +42,21 @@ public class HomeRouter extends HttpServlet{
         @Override
         protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        try {
             home(request, response);
+        } catch (SQLException ex) {
+            Logger.getLogger(HomeRouter.class.getName()).log(Level.SEVERE, null, ex);
+        }
         }
         
         @Override
         protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        try {
             home(request, response);
+        } catch (SQLException ex) {
+            Logger.getLogger(HomeRouter.class.getName()).log(Level.SEVERE, null, ex);
+        }
         }
 }
 
