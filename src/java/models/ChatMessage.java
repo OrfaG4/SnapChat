354af7c117650b7bb1dd727b9d679b5783f7dbec
J0eLGK <GEORGE_TAME@HOTMAIL.COM>
2015-12-14 02:42:25 +0200

    Update ChatMessage.java

--- src/java/models/ChatMessage.java
+++ src/java/models/ChatMessage.java
@@ -1,38 +1,123 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package models;
 
+import java.io.Serializable;
 
-public class ChatMessage {
-	private String message;
-	private String sender;
-	private String received;
+
+public class ChatMessage implements Serializable {
+    public static int NEW_MESSAGE_TYPE=0;
+    public static int RETURN_MESSAGE_TYPE=1;
+    private String message;
+    private int from_user;
+    private int to_user;
+    private String from_user_name;
+    private String messageTime;
+    private String b64;
+    private int messageType=0;
+    public String toXML(){
+        return "<chat_message><from_user>"+from_user+"</from_user><from_user_name>"+from_user_name+"</from_user_name><message>"+message+"</message><message_type>"+messageType+"</message_type></chat_message>";
+    }
+    public String toMultipleValues(){
+        return from_user+"ø"+from_user_name+"ø"+message+"ø"+b64+"ø";
+    }
+    public void returnMessage(){
+        int temp=this.from_user;
+        this.from_user=this.to_user;
+        this.to_user=temp;
+        this.messageType=1;
+    }
+    
+        
+    public String getB64() {
+        return b64;
+    }
+    public void setB64(String b64){
+        this.b64 = b64;
+    }
 
     public String getMessage() {
         return message;
     }
 
+    /**
+     * @param message the message to set
+     */
     public void setMessage(String message) {
         this.message = message;
+        //prevent html tags
+        this.message=this.message.replaceAll("<", "&lt;");
+        //prevent special split charachter
+        this.message=this.message.replaceAll("ø", "");
     }
 
-    public String getSender() {
-        return sender;
+    /**
+     * @return the from_user
+     */
+    public int getFrom_user() {
+        return from_user;
     }
 
-    public void setSender(String sender) {
-        this.sender = sender;
+    /**
+     * @param from_user the from_user to set
+     */
+    public void setFrom_user(int from_user) {
+        this.from_user = from_user;
     }
 
-    public String getReceived() {
-        return received;
+    /**
+     * @return the to_user
+     */
+    public int getTo_user() {
+        return to_user;
     }
 
-    public void setReceived(String received) {
-        this.received = received;
+    /**
+     * @param to_user the to_user to set
+     */
+    public void setTo_user(int to_user) {
+        this.to_user = to_user;
     }
-        
+
+    /**
+     * @return the from_user_name
+     */
+    public String getFrom_user_name() {
+        return from_user_name;
+    }
+
+    /**
+     * @param from_user_name the from_user_name to set
+     */
+    public void setFrom_user_name(String from_user_name) {
+        this.from_user_name = from_user_name;
+    }
+
+    /**
+     * @return the messageTime
+     */
+    public String getMessageTime() {
+        return messageTime;
+    }
+
+    /**
+     * @param messageTime the messageTime to set
+     */
+    public void setMessageTime(String messageTime) {
+        this.messageTime = messageTime;
+    }
+
+    /**
+     * @return the messageType
+     */
+    public int getMessageType() {
+        return messageType;
+    }
+
+    /**
+     * @param messageType the messageType to set
+     */
+    public void setMessageType(int messageType) {
+        this.messageType = messageType;
+    }
+    
 }
