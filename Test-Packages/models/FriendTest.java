410e06c5bae931d58f066bb19469ef745b4e9e35
dimitrisKK <jim_karad@hotmail.gr>
2015-12-16 22:42:13 +0200

    Update FriendTest.java

--- Test-Packages/models/FriendTest.java
+++ Test-Packages/models/FriendTest.java
@@ -11,10 +11,14 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
 
-
+/**
+ *
+ * @author dimitrisKK
+ */
 public class FriendTest {
-    
+    Friend mockObj = new Friend("10","11");
     public FriendTest() {
     }
     
@@ -28,13 +32,36 @@ public class FriendTest {
     
     @Before
     public void setUp() {
+        mockObj = mock(Friend.class);
     }
     
     @After
     public void tearDown() {
     }
 
-   
+    /**
+     * Test of getMyId method, of class Friend.
+     */
+    @Test
+    public void testGetMyId() {
+        System.out.println("getMyId");
+        Friend instance = new Friend("1","2");
+        String expResult = "1";
+        String result = instance.getMyId();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of getFriendId method, of class Friend.
+     */
+    @Test
+    public void testGetFriendId() {
+        System.out.println("getFriendId");
+        Friend instance = new Friend("1","2");
+        String expResult = "2";
+        String result = instance.getFriendId();
+        assertEquals(expResult, result);
+    }
 
     /**
      * Test of setMyId method, of class Friend.
@@ -42,31 +69,33 @@ public class FriendTest {
     @Test
     public void testSetMyId() {
         System.out.println("setMyId");
-        String myId = "12";
+        String myId = "1";
         Friend instance = new Friend();
         instance.setMyId(myId);
-        String exp=instance.getMyId();
-        assertEquals(myId,exp);
+        assertEquals(myId, instance.getMyId());
     }
-
-    
-    
-    
-
 
     /**
-     * Test of setStatus method, of class Friend.
+     * Test of setFriendId method, of class Friend.
      */
     @Test
-    public void testSetStatus() {
-        System.out.println("setStatus");
-        String status = "0";
+    public void testSetFriendId() {
+        System.out.println("setFriendId");
+        String friendId = "2";
         Friend instance = new Friend();
-        instance.setStatus(status);
-        String exp = instance.getStatus();
-        assertEquals(status,exp);
-        
-     
+        instance.setFriendId(friendId);
+        assertEquals(friendId, instance.getFriendId());
     }
 
+    /**
+     * Test of addFriend method, of class Friend.
+     */
+    @Test
+    public void testAddFriend() {
+        System.out.println("addFriend");
+        int myId = 1;
+        int friendId = 2;
+        mockObj.addFriend(myId, friendId);
+    }
+    
 }
