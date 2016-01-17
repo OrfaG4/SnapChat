086552366e9a5d44ae21d77759b14309c7a97e45
J0eLGK <GEORGE_TAME@HOTMAIL.COM>
2016-01-18 00:09:57 +0200

    Update LoginController.java

--- src/java/controllers/LoginController.java
+++ src/java/controllers/LoginController.java
@@ -11,28 +11,32 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.RequestDispatcher;
-import models.User;
+import models.ChatUser;
 import javax.servlet.http.HttpSession;
 
 public class LoginController extends HttpServlet {
         protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException, SQLException  {
             response.setContentType("text/html;charset=UTF-8");
-            ArrayList<User> friends = new ArrayList<User>();
+            ArrayList<ChatUser> friends = new ArrayList<ChatUser>();
             PrintWriter pw = response.getWriter();
             try {
-                if(User.LoginUser(request.getParameter("username"),request.getParameter("password"))){
-                    User user = new User();
+                if(ChatUser.LoginUser(request.getParameter("username"),request.getParameter("password"))){
+                    ChatUser user = new ChatUser();
                     user.setUsername(String.valueOf(request.getParameter("username")));
                     user.getUser();
 
                     HttpSession sessionUser = request.getSession();
-                    sessionUser.setAttribute("username",user.getUsername());
-                    sessionUser.setAttribute("id", user.getId());
-                    friends = user.getAllMyFriends();
-                    request.setAttribute("friends", friends);
-                    RequestDispatcher rd1 = request.getRequestDispatcher("views/home.jsp");
-                    rd1.forward(request,response);     
+                    pw.println("success");
+                    if(sessionUser != null){
+                        sessionUser.setAttribute("username",user.getUsername());
+                        sessionUser.setAttribute("id", user.getId());
+                        friends = user.getAllMyFriends();
+                        sessionUser.setAttribute("friendsList", friends);
+                        request.setAttribute("friends", friends);
+                        RequestDispatcher rd1 = request.getRequestDispatcher("views/home.jsp");
+                        rd1.forward(request,response);
+                    }
                 }  
                 else{
                     pw.println("Λανθασμένος συνδυασμός ονόματος χρήστη/κωδικού πρόσβασης");
